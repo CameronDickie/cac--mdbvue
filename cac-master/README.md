121a2f6e91a5152b75745c2d4b348bccdba6c7fa
Cameron Dickie <cameronbdickie@gmail.com>
2020-06-13 16:31:38 -0400

    changes to header
    header content

---
+++ cac-master/README.md
@@ -0,0 +1,2 @@
+# cac
+i swear to god i shouldn't have to make this one more time if i do i would rather be dead
---
+++ src/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <!-- NAVBAR COMPONENTS -->
+    <mdb-navbar color="white" style="min-height:75px;" light>
+      <mdb-navbar-brand href="https://mdbootstrap.com/">Climate Action Carleton</mdb-navbar-brand>
+      <mdb-navbar-toggler>
+        <mdb-navbar-nav>
+          <mdb-nav-item href="#" active>Home</mdb-nav-item>
+          <mdb-nav-item href="#">About</mdb-nav-item>
+          <mdb-nav-item href="#">Contact</mdb-nav-item>
+        </mdb-navbar-nav>
+      </mdb-navbar-toggler>
+    </mdb-navbar>
+</template>
+
+<script>
+import {
+  mdbNavbar,
+  mdbNavbarBrand,
+  mdbNavbarToggler,
+  mdbNavbarNav,
+  mdbNavItem
+} from "mdbvue";
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
